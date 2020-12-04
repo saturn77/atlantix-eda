92730fadaf5bfa4e7f3822accaf32cb95e1a9b13
James Bonanno <james@atlantixeng.com>
2020-12-03 23:08:21 -0500

    rename lib.rs function to component in toml file

--- examples/gen_vishay_resistor.rs
+++ examples/gen_vishay_resistor.rs
@@ -36,24 +36,24 @@
 //! PADS was tested with an older version of this software so it should easily be adopted.
 //! Tools such as the [KiCad]( https://gitlab.com/kicad/libraries/kicad-library-utils) generator functionality could
 //! be replaced by Atlantix-EDA generate module, with the help of templating to take care of
-//! boilerplate code in the resistor library. A template engine such as [Askama](https://djc.github.io/askama/) would be a good
+//! boilerplate code in the component::Resistor library. A template engine such as [Askama](https://djc.github.io/askama/) would be a good
 //! approach.
 //!
-//! Overall, resistor libraries for Altium are included as part of this repo that include
+//! Overall, component::Resistor libraries for Altium are included as part of this repo that include
 //! every E-96 value from 0201 to 2512, which is over 5,000 parts.
 //!
 //! # Background
 //!
-//! For a complete resistor library from 0201 to 2512 in the E-96 1% series there are 5,184 resistor values. The ability to generate these libraries on the fly
+//! For a complete component::Resistor library from 0201 to 2512 in the E-96 1% series there are 5,184 component::Resistor values. The ability to generate these libraries on the fly
 //! and change any data associated with each part is quite powerful.
 //!
-//! The use of Atlantix-EDA to generate the resistor library enables engineers developing in tools such as Altium, KiCad, etc. to have a comprehensive library 
+//! The use of Atlantix-EDA to generate the component::Resistor library enables engineers developing in tools such as Altium, KiCad, etc. to have a comprehensive library 
 //! available to them. This library enables quicker design cycles by
 //! eliminating part creation and more robust design as BOM generation from the schematic will be accurate and true.
 //!
 //! ![image](../table.jpg)
 //!
-//! A history and more complete background of the [resistor series.](https://en.wikipedia.org/wiki/E_series_of_preferred_numbers "Wikipedia resistor series background")
+//! A history and more complete background of the [component::Resistor series.](https://en.wikipedia.org/wiki/E_series_of_preferred_numbers "Wikipedia component::Resistor series background")
 //!
 //!
 //! ## Basic usage example
@@ -62,14 +62,14 @@
 //!
 //! ```
 //! let decades = [1,10,100,1000,10000,100000];
-//! let mut r0603 = generate::Resistor::new(96, "0603".to_string());
+//! let mut r0603 = generate::component::Resistor::new(96, "0603".to_string());
 //!
 //!	for decade in decades.iter() {
 //!		r0603.generate(*decade);
 //!	}
 //! ```
 //! ## Detailed usage example
-//! The code below shows how to actually instantiate the resistor object and generate
+//! The code below shows how to actually instantiate the component::Resistor object and generate
 //! values across several decade values while writing to a file, which is then imported
 //! by Altium or another tool.
 //!
@@ -78,7 +78,7 @@
 //! 		let decades = [1,10,100,1000,10000,100000];
 //!			let mut name_0402 : String = " ".to_string();
 //!
-//!			let mut r0402 = generate::Resistor::new(96, "0402".to_string());
+//!			let mut r0402 = generate::component::Resistor::new(96, "0402".to_string());
 //!
 //!			for decade in decades.iter() {
 //!				name_0402 = r0402.generate(*decade);
@@ -92,7 +92,7 @@
 //!
 //!			file.write_all(name_0402.as_bytes()).expect("write failed");
 //!
-//!			println!("** Success::0402 resistor generation.");
+//!			println!("** Success::0402 component::Resistor generation.");
 //! 	}
 //! ```
 //!
@@ -105,8 +105,9 @@
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 
+
 //============================================================
-//  Main::Creating Programmatically Generated Resistor Library
+//  Main::Creating Programmatically Generated component::Resistor Library
 //============================================================
 
 /// The main module which instantiates the generate module as an object. The generator can yield values over decade ranges.
@@ -121,7 +122,7 @@ fn main() -> std::io::Result<()> {
 
     //--------------------------------------------------------------
 
-    let mut r0402 = generate::Resistor::new(96, "0402".to_string());
+    let mut r0402 = component::Resistor::new(96, "0402".to_string());
     for decade in decades.iter() {
         name_0402 = r0402.generate(*decade);
         //println!("{ }",name_0402)
@@ -137,47 +138,47 @@ fn main() -> std::io::Result<()> {
     file.write_all("Part,Description,Value,Case,Power,Supplier 1,Supplier Part Number 1,Library Path,Library Ref,Footprint Path,Footprint Ref,Company,Comment \r\n".as_bytes()).expect("write failed");
 
     file.write_all(name_0402.as_bytes()).expect("write failed");
-    println!("** Success::0402 resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
+    println!("** Success::0402 component::Resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
 
     //--------------------------------------------------------------
 
-    let mut r0603 = generate::Resistor::new(96, "0603".to_string());
+    let mut r0603 = component::Resistor::new(96, "0603".to_string());
     for decade in decades.iter() {
         name_0603 = r0603.generate(*decade);
         //println!("{ }",name_0603)
     }
     file.write_all(name_0603.as_bytes()).expect("write failed");
-    println!("** Success::0603 resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
+    println!("** Success::0603 component::Resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
 
     //--------------------------------------------------------------
 
-    let mut r0805 = generate::Resistor::new(96, "0805".to_string());
+    let mut r0805 = component::Resistor::new(96, "0805".to_string());
     for decade in decades.iter() {
         name_0805 = r0805.generate(*decade);
         //println!("{}",name_0805);
     }
     file.write_all(name_0805.as_bytes()).expect("write failed");
-    println!("** Success::0805 resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
+    println!("** Success::0805 component::Resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
 
     //--------------------------------------------------------------
 
-    let mut r1206 = generate::Resistor::new(96, "1206".to_string());
+    let mut r1206 = component::Resistor::new(96, "1206".to_string());
     for decade in decades.iter() {
         name_1206 = r1206.generate(*decade);
         //println!("{}",name_0805);
     }
     file.write_all(name_1206.as_bytes()).expect("write failed");
-    println!("** Success::1206 resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
+    println!("** Success::1206 component::Resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
 
     //--------------------------------------------------------------
 
-    let mut r1210 = generate::Resistor::new(96, "1210".to_string());
+    let mut r1210 = component::Resistor::new(96, "1210".to_string());
     for decade in decades.iter() {
         name_1210 = r1210.generate(*decade);
         //println!("{}",name_0805);
     }
     file.write_all(name_1210.as_bytes()).expect("write failed");
-    println!("** Success::1210 resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
+    println!("** Success::1210 component::Resistor E-96 series generation (1 Ohm to 1 MOhm).\r\n");
 
     //--------------------------------------------------------------
     Ok(())
